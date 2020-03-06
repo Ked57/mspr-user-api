5867ba3d77c45f01116d170da49b47ebbba1452b
Ked57 <kedketh@gmail.com>
2020-03-06 11:38:03 +0100

    Bump test coverage to pass CI

--- tests/unit/user.ts
+++ tests/unit/user.ts
@@ -1,11 +1,18 @@
 import test from "ava";
+import { of } from "await-of";
 import { userHandler } from "../../src/handlers/userHandler";
-import { userFindOneMock, userMock } from "../../mock/user";
+import {
+  userFindOneMock,
+  userMock,
+  userFindOneThrowMock
+} from "../../mock/user";
 
 test("/user/{id} returns an user", async t => {
   const paramId = "1";
-  const result = await userHandler(paramId, userFindOneMock);
-  t.log("result", result);
+  const [result, err] = await of(userHandler(paramId, userFindOneMock));
+  if (err) {
+    t.fail(err.message);
+  }
   t.deepEqual(
     result,
     userMock.find(value => value.auth_id === paramId)
@@ -14,7 +21,29 @@ test("/user/{id} returns an user", async t => {
 
 test("/user/{id} fails if a wrong id is passed", async t => {
   const paramId = "not ok";
-  const result = await userHandler(paramId, userFindOneMock);
-  t.log("result", result);
+  const [result, err] = await of(userHandler(paramId, userFindOneMock));
+  if (err) {
+    t.fail(err.message);
+  }
   t.deepEqual(result.message, "wrong id provided");
+});
+
+test("/user/{id} fails if no id is passed", async t => {
+  const paramId = undefined;
+  const [result, err] = await of(userHandler(paramId as any, userFindOneMock));
+  if (err) {
+    t.fail(err.message);
+  }
+  t.deepEqual(result.message, "no id provided");
+});
+
+test("userHandler can handle orm errors", async t => {
+  const paramId = "1";
+  const [result, err] = await of(
+    userHandler(paramId as any, userFindOneThrowMock)
+  );
+  if (err) {
+    t.fail(err.message);
+  }
+  t.deepEqual(result.message, "Error: { name : error message: test }");
 });
