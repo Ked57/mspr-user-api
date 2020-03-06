706f6dbe8c00833e8f2c9fada67ec86a61cce1c7
Clément Fassot <kedketh@gmail.com>
2020-03-06 15:51:50 +0100

    :sparkles: Add post user route (#29)

--- tests/integration/user.ts
+++ tests/integration/user.ts
@@ -15,7 +15,7 @@ test.before(async t => {
   (t.context as any)["server"] = app;
   (t.context as any)["baseUrl"] = address;
   const prisma = new PrismaClient();
-  prisma.user.upsert({
+  await prisma.user.upsert({
     create: {
       auth_id: "1",
       user_name: "bobo cargo"
@@ -49,5 +49,35 @@ test("/user returns a 404 error", async t => {
   t.assert(
     err.statusCode === 404,
     `Expected /user result to be a 404 error, instead got ${err}`
+  );
+});
+
+test("POST /user returns a 201", async t => {
+  const response = await fetch(`http://localhost:${port}/user`, {
+    method: "POST",
+    body: JSON.stringify({
+      auth_id: "testuser" + Math.random() * 10000,
+      user_name: "testuser"
+    })
+  });
+  t.assert(
+    response.status === 201,
+    `Expected POST /user result to have status of 201, instead got ${
+      response.status
+    } : ${JSON.stringify(await response.json())}`
+  );
+});
+
+test("POST /user returns a 400 when given a bad payload", async t => {
+  const response = await fetch(`http://localhost:${port}/user`, {
+    method: "POST",
+    body: JSON.stringify({
+      authId: "testuser" + Math.random() * 10000,
+      userName: "testuser"
+    })
+  });
+  t.assert(
+    response.status === 400,
+    `Expected POST /user result to have status of 400, instead got ${response.status}, response ${response.body}`
   );
 });
